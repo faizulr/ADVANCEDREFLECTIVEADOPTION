544f65fc1a6b081461764756b157a24679288b47
faizulm <43180194+faizulm@users.noreply.github.com>
2018-09-19 15:04:56 -0400

    Average of three numbers
    Calculate the average of three numbers.

---
+++ pythonexamples/avgofthreenumbers.py
@@ -0,0 +1,12 @@
+# Get three test score
+num1 = int(raw_input("Enter first number "))
+
+num2 = int(raw_input("Enter second number "))
+
+num3 = int(raw_input("Enter third number "))
+   
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# Print the average of three numbers
+print "the average of the three numbers that you entered is: ", average 
